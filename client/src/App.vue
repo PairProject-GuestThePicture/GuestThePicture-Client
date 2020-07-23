07e1fe63bea9077e6f16e65b2eb4d9484b9f0461
Nurfiah Idris <nurfiahidris@Nurfiahs-MacBook-Air.local>
2020-07-23 23:22:58 +0800

    fromNurfiah

---
+++ client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+<script>
+import io from 'socket.io-client'
+export default {
+  name: 'App',
+  data () {
+    return {
+    }
+  },
+  mounted () {
+    this.socket = io.connect('http://localhost:3000')
+    // received event from server
+    this.socket.on('init', function (payload) {
+      console.log(payload)
+    })
+  },
+  methods: {
+    send () {
+      this.socket.emit('send-message', { content: this.content })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ client/src/components/CreateRoom.vue
@@ -0,0 +1,72 @@
+<template>
+<div>
+  <button @click.prevent="showModal" data-toggle="modal" data-target="#exampleModal">Create Room</button>
+  <!-- Button trigger modal -->
+
+<!-- Modal -->
+<div v-bind:class="{modal: true, fade: true, in: isModalOpen}" v-bind:style="{display: display}" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h2 class="modal-title" id="exampleModalLabel">Rooms' Name</h2>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close" @click.prevent="close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div>
+          <label for="title"></label>
+          <input type="text" id="title" v-model="title" placeholder="title">
+        </div>
+        <div>
+          <label for="description"></label>
+          <input type="text" id="description" v-model="description" placeholder="description">
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal" @click.prevent="close">Close</button>
+        <button type="button" class="btn btn-primary" @click.prevent="save">Save</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'CreateRoom',
+  data () {
+    return {
+      title: '',
+      description: '',
+      isModalOpen: false,
+      display: 'none'
+    }
+  },
+  methods: {
+    showModal () {
+      this.isModalOpen = true
+      this.display = 'block'
+    },
+    close () {
+      this.isModalOpen = false
+      this.display = 'none'
+    },
+    save () {
+    }
+  }
+}
+</script>
+
+<style scoped>
+  button{
+    width:100px;
+  }
+  input{
+    width: 300px;
+    margin:20px;
+    height: 30px;
+  }
+</style>
