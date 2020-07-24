d5559023bdee2476435a1036c6eb4453f0421983
ichlasulamalgit <id.ichlasulamal@gmail.com>
2020-07-24 11:02:09 +0700

    Login handle: Mengirim username ke store

--- client/src/components/Chat.vue
+++ client/src/components/Chat.vue
@@ -2,14 +2,28 @@
   <div class="chat">
     <ul id="messages"></ul>
     <form action="">
-      <input id="m" autocomplete="off" /><button>Send</button>
+      <input type="text" v-model="message" autocomplete="off" placeholder="Type Message Here ..."/><button>Send</button>
     </form>
   </div>
 </template>
 
 <script>
-export default {
+import io from 'socket.io-client'
 
+export default {
+  name: 'Chat',
+  data () {
+    return {}
+  }, 
+  mounted () {
+    this.socket = io.connect('http://localhost:3000')
+
+    this.socket.on('init', function () {
+      console.log('Init invoked')
+    })
+
+    
+  }
 }
 </script>
 
